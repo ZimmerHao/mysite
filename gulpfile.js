4fc10cd47207f70963269fe3be22417cd3c8bf83
haojinming <haojinming@breadtrip.com>
2015-08-21 13:13:26 +0800

    init concat notify rename uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
 var concat = require('gulp-concat');
-
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var notify = require("gulp-notify");
 
 
 gulp.task('js', function() {
@@ -9,5 +11,9 @@ gulp.task('js', function() {
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./mysite/assets/dist/js/'));
+    .pipe(gulp.dest('./mysite/assets/dist/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./mysite/assets/dist/js/'))
+    .pipe(notify({ message: 'js task complete' }));
 });
