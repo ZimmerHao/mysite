a2badbc5a91aecd01ce60e5e319670fb108c7c47
zimmer <zimmer.hao@gmail.com>
2015-09-02 21:43:45 +0800

    use requirejs text plugin for template

--- mysite/assets/src/js/main.js
+++ mysite/assets/src/js/main.js
@@ -21,6 +21,7 @@ require.config({
 		jquery: '../../components/jquery/dist/jquery',
 		underscore: '../../components/underscore/underscore',
 		backbone: '../../components/backbone/backbone',
+		text: '../../components/text/text'
 	}
 });
 
--- mysite/assets/src/js/views/book.js
+++ mysite/assets/src/js/views/book.js
@@ -1,13 +1,18 @@
 /**
  * Created by jinming on 15/9/1.
  */
-define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
-   'use strict';
+define(['jquery',
+        'underscore',
+        'backbone',
+        'text!templates/book-overview.html'
+], function($, _, Backbone, bookItemOverviewTemplate) {
+
+    'use strict';
 
     var BookSummaryView = Backbone.View.extend({
         tagName: "div",
 
-        template: _.template($('#book-item-overview-template').html()),
+        template: _.template(bookItemOverviewTemplate),
 
         initialize: function() {
           this.listenTo(this.model, 'change', this.render);
